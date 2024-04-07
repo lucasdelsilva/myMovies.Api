ae1f3cf9051a5d70ec757f663c400ebe5fe627b0
lucasdelsilva <silva.lucasdel@outlook.com>
2024-04-07 16:19:04 -0300

    Estrutura: models, controller e context

---
+++ Context/Data/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyMovies.Api.Domain.Models;
+
+namespace MyMovies.Api.Context.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<Movie> Filmes { get; set; }
+    }
+}
---
+++ Domain/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyMovies.Api.Domain.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Director { get; set; }
+        public List<string> Gender { get; set; }
+        public List<string> Actors { get; set; }
+        public int Duration { get; set; }
+        public DateTime Year { get; set; }
+    }
+}
